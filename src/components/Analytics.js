36285966c201b13a7b61eda85f8145668e1a7d6c
Alexander Madyankin <alexander@madyankin.name>
2017-09-05 08:47:11 +0300

    Fix analytics

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const Analytics = () => (
-  <script>
-    {`
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-38376101-1', 'auto');
-      ga('send', 'pageview');
-		`}
-  </script>
-);
+const script = `
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-38376101-1', 'auto');
+  ga('send', 'pageview');
+`;
+
+const Analytics = () => <script dangerouslySetInnerHTML={{ __html: script }} />;
 
 export default Analytics;
